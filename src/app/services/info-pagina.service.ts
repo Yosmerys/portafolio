334940f93f7211c8701ccbb4de746fa6c61f0440
yosmery <lmlarelis@gmail.com>
2022-04-20 20:37:07 -0400

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,28 +1,46 @@
 import { HttpClient } from '@angular/common/http'; //peticion json
 import { Injectable } from '@angular/core';
 import { InfoPagina } from '../interfaces/info-pagina.interfaces';
+
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
-  info:InfoPagina={};
 
+  info:InfoPagina={};
   cargada=false;
+  equipo:any[]=[];
 
   constructor(private http:HttpClient) {
   //console.log("Servicio de info pagina listo");
   /* LEER JSON */
-  this.http.get('assets/data/data-pagina.json').subscribe(
-    (resp:InfoPagina)=>{
-     // console.log(resp);
-     this.cargada=true;
-     this.info=resp;
-
-      console.log( resp );
-
-    }
-  );
+this.cargarInfo();
+this.cargarEquipo();
 
 
    }
+   private cargarInfo(){     
+    this.http.get('assets/data/data-pagina.json').subscribe(
+      (resp:InfoPagina)=>{
+       this.cargada=true;
+       this.info=resp;
+       // console.log( resp );
+  
+      });
+   }
+
+private cargarEquipo(){
+  this.http.get('https://angular-html-d4c22-default-rtdb.firebaseio.com/equipo.json').subscribe(
+    (resp:any)=>{
+    
+      this.equipo=resp;
+     /*  console.log("mi respuesta",resp); */
+
+
+    });
+
+}
+
+
+
 }
